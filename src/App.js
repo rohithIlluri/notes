62aec343acc05f44162042f95bb5a822a17cd216
rohith illuri <rohith.illuri@gmail.com>
2021-03-27 15:12:02 +0530

    note

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import {useState} from "react";
+import uuid from "react-uuid";
+import './App.css';
+import Sidebar from "./Sidebar";
+import Main from "./Main";
+
+
+function App() {
+  const [notes, setNotes] = useState([]);
+
+  const [actibeNote, setActiveNote] = useState(false);
+   
+  const onAddNote = () => {
+     const newNote = {
+       id: uuid(),
+
+       title: "Untitled Note",
+
+       body: "",
+
+       lastModified: Date.now(),
+     }
+ 
+
+  setNotes([ newNote, ...notes]);
+  
+    };
+
+    const onDeleteNote = (idToDelete) => {
+      setNotes(notes.filter((note) => note.id !== idToDelete))
+    };
+
+  return (
+    <div className="App">
+      <Sidebar notes={notes} 
+      onAddNote={onAddNote}
+      onDeleteNote={onDeleteNote}
+      activeNote={actibeNote}
+      setActiveNote={setActiveNote}/>
+      <Main />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Main.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function Main() {
+    return (
+        <div className="app-main">
+            <div className="app-main-note-edit">
+             <input type="text" id="title" autoFocus/>
+             <textarea id="body" placeholder="write your notes here ..."/>
+            </div>
+
+            <div className="app-main-note-preview">
+              <h1 className="preview-title">TITLE</h1>
+              <div className="markdown-preview">note preview</div>
+              
+            </div>
+        </div>
+    )
+}
+
+export default Main
